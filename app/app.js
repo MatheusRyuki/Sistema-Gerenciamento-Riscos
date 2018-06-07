37d81eb0c12427c682b0943a8bb39b8dba9bb55d
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-06-07 13:28:54 +0000

    Servidor Node.js Pronto

--- app/app.js
+++ app/app.js
@@ -2,11 +2,20 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+var expressLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs'); 
+app.use(expressLayouts);         
+app.use(bodyParser.urlencoded());
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('../views/index');
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+app.use(express.static(__dirname + '/public'));
