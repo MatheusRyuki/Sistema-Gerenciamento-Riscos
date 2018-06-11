5265e0fd8c2c5ee33bc5115baecb6ee21d7940ac
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-06-11 12:31:04 +0000

    Mudança para usar arquivos locais.

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.set('view engine', 'ejs');        
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');        
+app.use(express.static(__dirname + "/public"));
+console.log(__dirname);
 
 
 app.get('/', function (req, res) {
-  res.render('../views/index');
+  res.render('/views/index');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Servidor funcionando"');
 });
 
